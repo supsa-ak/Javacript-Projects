d0e5d458e842e8790d79bcddb69557095f45aee7
Sarthak <sarthakkalpande113@gmail.com>
2021-09-05 01:05:30 +0530

    Added delete and clear all functions to todolistjs app

--- Projects/4-9-2021/todolistjs/index.js
+++ Projects/4-9-2021/todolistjs/index.js
@@ -1,31 +1,42 @@
 // console.log('hi');
 
-setInterval(display,500);
+setInterval(display, 500);
 
-var task = ['go to meet', 'join lect', 'send hi to onkar'];
+var task = ['go to meet', 'join lect', 'take notes'];
 
-function add(){
-    let work = document.querySelector('#task-input').value; 
+function add() {
+    let work = document.querySelector('#task-input').value;
     task.unshift(work);
+    document.querySelector('#task-input').value = '';
 }
 
-function display(){
+function display() {
     document.getElementById("display-div").innerHTML = "";
     for (let i = 0; i < task.length; i++) {
         var p = document.createElement("p");
+        p.id = "task-"+i;
         p.innerHTML = task[i]
         document.getElementById("display-div").appendChild(p);
-        
+
+        var u = document.createElement('button');
+        u.innerHTML = "Update";
+        u.onclick = async function(){
+            var update = document.getElementById('task-'+i);
+            tag = document.createElement('input');
+            update.replaceWith(tag);
+            task[i] = tag.value;
+        };
+        document.getElementById("display-div").appendChild(u);        
+
         var d = document.createElement("button");
-        d.id= i;
         d.innerHTML = "Delete";
-        d.onclick =  deleteFun(i);        
+        d.onclick = function () {
+            task.splice(i, 1);
+        };
         document.getElementById("display-div").appendChild(d);
-        
     }
 }
 
-function deleteFun(index){
-    console.log('hi man');
-    
+function clr() {
+    task = [];
 }
